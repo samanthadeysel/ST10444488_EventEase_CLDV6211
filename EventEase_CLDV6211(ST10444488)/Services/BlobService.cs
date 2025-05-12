b2850fbf38deae1ebc383b74ef038afbc39609e3
samanthadeysel <st10444488@vcconnect.edu.za>
2025-05-12 15:56:40 +0200

    Blob Service Instance added

---
+++ EventEase_CLDV6211(ST10444488)/Services/BlobService.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
+namespace EventEase_CLDV6211_ST10444488_.Services
+{
+    public class BlobService
+    {
+        private readonly BlobContainerClient _containerClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureBlobStorage:ConnectionString"];
+            var containerName = configuration["AzureBlobStorage:ContainerName"];
+            _containerClient = new BlobContainerClient(connectionString, containerName);
+        }
+
+        public async Task<string> UploadFileAsync(IFormFile file)
+        {
+            var blobName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+            var blobClient = _containerClient.GetBlobClient(blobName);
+
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+
+            return blobClient.Uri.ToString();
+        }
+    }
+}
